d9291a0314c2396b6974c8ff543162e503076999
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-09-20 20:01:28 -0400

    feat: Install bootstrap

---
+++ bootstrap-try/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const h1Element = document.createElement('div');
+h1Element.classList.add('container-fluid');
+
+document.body.appendChild(h1Element);
